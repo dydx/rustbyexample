fa7d8042301a3c14d7dd3f0b9472c01adb95f752
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-05-27 19:23:05 -0400

    Debug example

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
+#[derive(Debug)]
+struct Structure(i32);
+
 fn main() {
-  println!("{} days", 31);
+  println!("{:?} months in a year", 12);
+  println!("{1:?} {0:?} is the {actor:?} name",
+    "Slater",
+    "Christian",
+    actor="actor's");
 
-  println!("{0}, this is {1}. {1} this is {0}",
-    "Alice", "Bob");
-
-  println!("{subject} {verb} {predicate}",
-    predicate="over the lazy dog",
-    subject="the quick brown fox",
-    verb="jumps");
-
-  println!("{} of {:b} people know binary, the other half don't",
-    1, 2);
-
-  println!("My name is {0}, {1} {0}",
-    "Bond", "James");
+  println!("Now {:?} will print!",
+    Structure(3));
 }
