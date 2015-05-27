7837a4b56cfcc0142feb650b0d2ecc7d33b972bd
Joshua Sandlin <joshua.sandlin@gmail.com>
2015-05-27 19:04:31 -0400

    Formatted print example

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
 fn main() {
-  let x = 5 + 5;
-  println!("is `x` 10 or 100? x = {}", x);
+  println!("{} days", 31);
+
+  println!("{0}, this is {1}. {1} this is {0}",
+    "Alice", "Bob");
+
+  println!("{subject} {verb} {predicate}",
+    predicate="over the lazy dog",
+    subject="the quick brown fox",
+    verb="jumps");
+
+  println!("{} of {:b} people know binary, the other half don't",
+    1, 2);
+
+  println!("My name is {0}, {1} {0}",
+    "Bond", "James");
 }
